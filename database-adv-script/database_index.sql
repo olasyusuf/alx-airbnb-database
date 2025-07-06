6f6dbf6aacabef729ebdf2d8255a69a3a2dca101
Olalekan Yusuf <olasyusuf@gmail.com>
2025-07-06 16:30:05 +0100

    Implement Indexes for Optimization - update!!

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -61,5 +61,25 @@ CREATE INDEX idx_property_amenity ON property_feature (amenity);
 CREATE INDEX idx_property_id ON property_feature (property_id);
 
 
+--- Query 1
+--- Find users with a specific email
+EXPLAIN ANALYZE SELECT user_id, first_name,last_name, email
+FROM user
+WHERE email = 'akpan_dan@gmail.com';
 
 
+--- Query 2
+--- Find properties in a specific location within a price range
+EXPLAIN ANALYZE SELECT property_id, name, location, price_per_night
+FROM property
+WHERE location = 'block 2A, Dimple Close, Ikoyi - Lagos' AND price_per_night <= 500
+ORDER BY price_per_night DESC;
+
+
+--- Query 3
+--- Find all confirmed bookings for a specific user
+EXPLAIN ANALYZE SELECT b.booking_id, p.name AS property_name, b.start_date, b.end_date, b.status
+FROM booking AS b
+INNER JOIN property AS p ON b.property_id = p.property_id
+WHERE b.user_id = '441de6b3-54cf-11f0-b651-0a0027000014' AND b.status = 'Confirmed'
+ORDER BY b.start_date ASC;
