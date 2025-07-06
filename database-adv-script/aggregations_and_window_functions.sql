893c9309dcafad6c74773d7d02a637ca2fc06f71
Olalekan Yusuf <olasyusuf@gmail.com>
2025-07-06 16:25:18 +0100

    Aggregations and Window Functions - update2!!

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -26,7 +26,8 @@ SELECT
     p.property_id,
     p.name AS property_name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_number
 FROM
     property AS p
 INNER JOIN
